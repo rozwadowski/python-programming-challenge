a8289da34a150233843ff78f0bc1b42e45f0146d
rozwadowski <siotsonek@gmail.com>
2018-03-26 10:19:06 +0200

    Add files via upload

---
+++ fibonacci_sequence/main.py
@@ -0,0 +1,13 @@
+
+def fibo(i):
+	if i == 0 or i == 1:
+		return i
+	else:
+		return fibo(i-1)+fibo(i-2)
+
+def main():
+	i = int(raw_input())
+	print fibo(i)
+	
+if __name__ == "__main__":
+    main()
