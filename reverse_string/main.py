39b61a8004cb15e4a39a01e8238f694cdaf4a046
rozwadowski <siotsonek@gmail.com>
2018-10-27 01:30:20 +0200

    Add files via upload

--- reverse_string/main.py
+++ reverse_string/main.py
@@ -1,13 +1,19 @@
 
 def reverse(s):
-	result = ""
-	for i in range(len(s)-1,-1,-1):
-		result = result + s[i]
-	return result
+    result = ""
+    for i in range(len(s)-1, -1, -1):
+        result = result + s[i]
+    return result
+
+
+def reverse2(s):
+    return s[::-1]
+
 
 def main():
-	s = str(raw_input())
-	print reverse(s)
-	
+    s = str(raw_input())
+    print reverse(s)
+
+
 if __name__ == "__main__":
     main()
